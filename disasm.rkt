cfd5db7fad9491a16e477f57c0c104560b08a826
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2012-09-14 22:10:35 -0400

    Shell out to disarm for disassembly of ARMv7

--- disasm.rkt
+++ disasm.rkt
@@ -1,5 +1,7 @@
 #lang racket/base
 
+(require (only-in racket/port with-input-from-bytes))
+(require (only-in racket/system system))
 (require ffi/unsafe)
 (require "platform.rkt")
 
@@ -19,15 +21,22 @@
 (define (disassemble-raw! x len
 			  #:arch [arch (current-cpu-architecture)]
 			  #:show-binary [show-binary #t])
-  (%disassemble-block x
-		      len
-		      (case arch
-			[(i386) 0]
-			[(x86_64) 1]
-			[else (error 'disassemble-block "Unsupported architecture ~v" arch)])
-		      (if show-binary 1 0)))
+  (if (eq? arch 'arm7)
+      (disassemble-arm7 x len)
+      (%disassemble-block x
+			  len
+			  (case arch
+			    [(i386) 0]
+			    [(x86_64) 1]
+			    [else (error 'disassemble-block "Unsupported architecture ~v" arch)])
+			  (if show-binary 1 0))))
 
 (define (disassemble-bytes! bs
 			    #:arch [arch (current-cpu-architecture)]
 			    #:show-binary [show-binary #t])
   (disassemble-raw! bs (bytes-length bs) #:arch arch #:show-binary show-binary))
+
+(define (disassemble-arm7 x len)
+  (with-input-from-bytes (subbytes x 0 len)
+    (lambda ()
+      (system "./disarm/disarm-0.11 - 0"))))
