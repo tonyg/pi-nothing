63b55e50ec2b3ce86cf37bd3937a573c223abdf5
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2012-10-08 18:08:59 -0400

    Copyright notice.

--- disasm.rkt
+++ disasm.rkt
@@ -1,4 +1,20 @@
 #lang racket/base
+;; Copyright (C) 2012 Tony Garnock-Jones <tonygarnockjones@gmail.com>
+;;
+;; This file is part of pi-nothing.
+;;
+;; pi-nothing is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation, either version 3 of the License,
+;; or (at your option) any later version.
+;;
+;; pi-nothing is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with pi-nothing. If not, see <http://www.gnu.org/licenses/>.
 
 (require (only-in racket/port with-input-from-bytes))
 (require (only-in racket/system system))
