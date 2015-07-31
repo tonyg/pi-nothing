d281722bb112b5c198ad25f18ed66e55bd199d94
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2015-07-31 12:02:19 -0400

    Add sys:invalidate-unified-cache, sys:clean-and-invalidate-data-cache, and sys:invalidate-tlb

--- main-arm.rkt
+++ main-arm.rkt
@@ -167,16 +167,19 @@
         (define-coprocessor-store 'sys:set-mmu-domains 15 0 3 0 0)
 
         ;; Cache management
-        (define-coprocessor-store 'sys:invalidate-instruction-cache 15 0 7 5 0)
+        (define-coprocessor-store 'sys:invalidate-instruction-cache    15 0 7 5 0)
         (label-anchor 'sys:instruction-sync-barrier)
         (label-anchor 'sys:instruction-mem-barrier)
-        (define-coprocessor-store 'sys:flush-prefetch-buffer 15 0 7 5 4)
-        (define-coprocessor-store 'sys:flush-branch-target-cache 15 0 7 5 6)
-        (define-coprocessor-store 'sys:invalidate-data-cache 15 0 7 6 0)
-        (define-coprocessor-store 'sys:clean-data-cache 15 0 7 10 0)
+        (define-coprocessor-store 'sys:flush-prefetch-buffer           15 0 7 5 4)
+        (define-coprocessor-store 'sys:flush-branch-target-cache       15 0 7 5 6)
+        (define-coprocessor-store 'sys:invalidate-data-cache           15 0 7 6 0)
+        (define-coprocessor-store 'sys:invalidate-unified-cache        15 0 7 7 0)
+        (define-coprocessor-store 'sys:clean-data-cache                15 0 7 10 0)
         ;; TODO: ARMv7-specific instruction replaces the next two definitions:
-        (define-coprocessor-store 'sys:data-sync-barrier 15 0 7 10 4)
-        (define-coprocessor-store 'sys:data-mem-barrier 15 0 7 10 5)
+        (define-coprocessor-store 'sys:data-sync-barrier               15 0 7 10 4)
+        (define-coprocessor-store 'sys:data-mem-barrier                15 0 7 10 5)
+        (define-coprocessor-store 'sys:clean-and-invalidate-data-cache 15 0 7 14 0)
+        (define-coprocessor-store 'sys:invalidate-tlb                  15 0 8 7 0)
 
         (label-anchor 'sys:interrupt-vector-undefined-instruction)
           0 0 0 0 ;; undefined instruction, normal ret returns to just after failing insn
